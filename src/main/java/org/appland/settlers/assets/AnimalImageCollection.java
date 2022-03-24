6816508d97c0af805dac187ee32a4f8e9a711b49
Johan Mårtenson <johan.martenson@gmail.com>
2022-03-24 19:36:27 +0100

    Set proper offses for the animal image atlas and re-use the convenience functions.

--- src/main/java/org/appland/settlers/assets/AnimalImageCollection.java
+++ src/main/java/org/appland/settlers/assets/AnimalImageCollection.java
@@ -30,29 +30,29 @@ public class AnimalImageCollection {
     }
 
     public void writeImageAtlas(String directory, Palette palette) throws IOException {
-        int maxHeight = 0;
-        int maxWidth = 0;
-        int maxImagesPerDirection = 0;
 
         // Find the max width and height, and the max number of images over all directions
+        Utils.RowLayoutInfo aggregatedLayoutInfo = new Utils.RowLayoutInfo();
+
         for (Direction direction : Direction.values()) {
             List<Bitmap> images = directionToImageMap.get(direction);
 
-            for (Bitmap bitmap : images) {
-                maxWidth = Math.max(bitmap.getWidth(), maxWidth);
-                maxHeight = Math.max(bitmap.getHeight(), maxHeight);
-            }
+            Utils.RowLayoutInfo layoutInfo = Utils.layoutInfoFromImageSeries(images);
 
-            maxImagesPerDirection = Math.max(maxImagesPerDirection, images.size());
+            aggregatedLayoutInfo.aggregate(layoutInfo);
         }
 
         // Write the image atlas, one row per direction, and collect metadata to write as json
-        Bitmap imageAtlas = new Bitmap(maxWidth * maxImagesPerDirection, maxHeight * Direction.values().length, palette, TextureFormat.BGRA);
+        Bitmap imageAtlas = new Bitmap(
+                aggregatedLayoutInfo.getRowWidth(),
+                aggregatedLayoutInfo.getRowHeight() * Direction.values().length,
+                palette,
+                TextureFormat.BGRA);
 
         JSONObject jsonImageAtlas = new JSONObject();
 
         // Fill in the images into the image atlas
-        int i = 0;
+        int directionIndex = 0;
         for (Direction direction : Direction.values()) {
 
             JSONObject jsonDirectionInfo = new JSONObject();
@@ -60,19 +60,25 @@ public class AnimalImageCollection {
             jsonImageAtlas.put(direction.name().toUpperCase(), jsonDirectionInfo);
 
             jsonDirectionInfo.put("startX", 0);
-            jsonDirectionInfo.put("startY", i * maxHeight);
-            jsonDirectionInfo.put("width", maxWidth);
-            jsonDirectionInfo.put("height", maxHeight);
+            jsonDirectionInfo.put("startY", directionIndex * aggregatedLayoutInfo.getRowHeight());
+            jsonDirectionInfo.put("width", aggregatedLayoutInfo.getImageWidth());
+            jsonDirectionInfo.put("height", aggregatedLayoutInfo.getImageHeight());
             jsonDirectionInfo.put("nrImages", directionToImageMap.get(direction).size());
+            jsonDirectionInfo.put("offsetX", aggregatedLayoutInfo.maxNx);
+            jsonDirectionInfo.put("offsetY", aggregatedLayoutInfo.maxNy);
 
-            int j = 0;
+            int imageIndex = 0;
             for (Bitmap image : directionToImageMap.get(direction)) {
-                imageAtlas.copyNonTransparentPixels(image, new Point(j * maxWidth, i * maxHeight), new Point(0, 0), image.getDimension());
+                imageAtlas.copyNonTransparentPixels(
+                        image,
+                        aggregatedLayoutInfo.getTargetPositionForImageInRow(image, directionIndex, imageIndex),
+                        new Point(0, 0),
+                        image.getDimension());
 
-                j = j + 1;
+                imageIndex = imageIndex + 1;
             }
 
-            i = i + 1;
+            directionIndex = directionIndex + 1;
         }
 
         imageAtlas.writeToFile(directory + "/" + "image-atlas-" + name.toLowerCase() + ".png");
